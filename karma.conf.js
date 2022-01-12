4a59049e756793830f9d292d3f4fb8abe202fd7b
minhnhan341 <97603659+minhnhan341@users.noreply.github.com>
2022-01-12 19:08:48 +0700

    Minh Nhân

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/untitled-app'),
+      dir: require('path').join(__dirname, './coverage/minh-nhn'),
       subdir: '.',
       reporters: [
         { type: 'html' },
